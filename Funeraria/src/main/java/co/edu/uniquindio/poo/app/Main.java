ddc4bb926efe5dd436a1a8b083a1db5dde5d07f2
camilaest <camiest57@gmail.com>
2025-09-02 23:31:18 -0500

    anexo main con menu crud cliente y empleado

--- Funeraria/src/main/java/co/edu/uniquindio/poo/app/Main.java
+++ Funeraria/src/main/java/co/edu/uniquindio/poo/app/Main.java
@@ -1,105 +1,272 @@
 package co.edu.uniquindio.poo.app;
 
 
-import co.edu.uniquindio.poo.model.*;
-
-import java.sql.SQLOutput;
-import java.time.LocalDate;
 import java.util.Scanner;
+
+import co.edu.uniquindio.poo.model.Cliente;
+import co.edu.uniquindio.poo.model.Empleado;
+import co.edu.uniquindio.poo.model.Funeraria;
+import co.edu.uniquindio.poo.model.TipoCargo;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Funeraria funeraria= Funeraria.getInstance();
 
-
-
-        //FALTA TERMINAR EL MAIN COMPLETO, REESTRUCTURARLO
-        //REVISA TODAS LAS CLASES Y ORGANIZALAS UN POCO PORFA
         int opcion;
+
         do {
-            System.out.println("\n===== MENÚ FUNERARIA =====");
-            System.out.println("1. Registrar cliente");
-            System.out.println("2. Registrar empleado");
-            System.out.println("3. Mostrar clientes");
-            System.out.println("4. Mostrar empleados");
-            System.out.println("5. Adquirir servicio");
-            System.out.println("6. Mostrar servicios adquiridos de un cliente");
-            System.out.println("0. Salir");
-            System.out.print("Opción: ");
+            System.out.println("===== MENÚ PRINCIPAL =====");
+            System.out.println("1. Soy Empleado");
+            System.out.println("2. Soy Cliente");
+            System.out.println("3. Soy Administrador");
+            System.out.println("4. Salir");
+            System.out.print("Seleccione una opción: ");
+            
             opcion = sc.nextInt();
-            sc.nextLine(); // limpiar buffer
 
             switch (opcion) {
-                case 1 -> {
-                    System.out.print("Nombre: ");
-                    String nombre = sc.nextLine();
-                    System.out.print("Apellido: ");
-                    String apellido = sc.nextLine();
-                    System.out.print("Identificación: ");
-                    String id = sc.nextLine();
-                    System.out.print("Email: ");
-                    String email = sc.nextLine();
-                    System.out.print("Año de nacimiento: ");
-                    int anio = sc.nextInt();
-                    System.out.print("Mes: ");
-                    int mes = sc.nextInt();
-                    System.out.print("Día: ");
-                    int dia = sc.nextInt();
-                    sc.nextLine();
+                case 1:
+                    if(iniciarSesion()){
+                        // menu del empleado
+                        mostrarMenuEmpleado(funeraria);
+                    }
+                    break;
+                case 2:
+                    if(iniciarSesion()){
+                        // menu del empleado
+                        mostrarMenuCliente(funeraria);
+                    }
+                    break;
+                case 3:
+                    if(iniciarSesion()){
+                        // menu del empleado
+                        mostrarMenuAdministrador(funeraria);
+                    }
+                    
+                    System.out.println("Saliendo del administrador...");
+                    break;
+                case 4:
+                    System.out.println("Saliendo del menú...");
+                    break;
+                default:
+                    System.out.println("⚠ Opción no válida, intenta de nuevo.");
+            }
+            System.out.println();
 
-                    // Usando Builder para cliente
-                    Cliente cliente = new Cliente.Builder()
-                            .nombre(nombre)
-                            .apellido(apellido)
-                            .identificacion(id)
+        } while (opcion != 3);
+
+        sc.close();
+    
+
+
+
+
+
+    }
+
+    private static void mostrarMenuAdministrador(Funeraria funeraria) {
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("===== MENÚ ADMINISTRADOR =====");
+            System.out.println("1. Registrar Empleado");
+            System.out.println("2. Actualizar Empleados");
+            System.out.println("3. Eliminar Empleados");
+            System.out.print("Seleccione una opción: ");
+            
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    
+
+                    System.out.println("Has elegido la opción: Registrar Empleado");
+                    Scanner scannerEmpleado= new Scanner(System.in);
+                    System.out.print("Ingrese el nombre del empleado: ");
+                    String nombreEmpleado = scannerEmpleado.nextLine();
+                    System.out.print("Ingrese el documento del cliente: ");
+                    String documentoCliente = scannerEmpleado.nextLine();
+                    System.out.print("Ingrese el email del cliente: ");
+                    String email = scannerEmpleado.nextLine();
+
+                     Empleado empleado= new Empleado.Builder()
+                            .nombre(nombreEmpleado)
+                            .identificacion(documentoCliente)
                             .email(email)
-                            .fechaNacimiento(LocalDate.of(anio, mes, dia))
-                            .build();
-
-                    funeraria.registrarCliente(cliente);
-                    System.out.println(cliente);
-                    System.out.println("Cliente registrado con éxito");
-                }
-                case 2 -> {
-                    System.out.print("Nombre: ");
-                    String nombre = sc.nextLine();
-                    System.out.print("Apellido: ");
-                    String apellido = sc.nextLine();
-                    System.out.print("Identificación: ");
-                    String id = sc.nextLine();
-                    System.out.print("Email: ");
-                    String email = sc.nextLine();
-                    System.out.print("");
-                    String cargo = sc.nextLine();
-
-                    // Usando Builder para empleado
-                    Empleado empleado = new Empleado.Builder()
-                            .nombre(nombre)
-                            .apellido(apellido)
-                            .identificacion(id)
-                            .email(email)
-                            .fechaNacimiento(LocalDate.now()) // de ejemplo
-                            .cargo(TipoCargo.valueOf(cargo.toUpperCase()))
                             .build();
 
                     funeraria.registrarEmpleado(empleado);
-                    System.out.println(empleado);
+                    System.out.println("Cliente registrado exitosamente.");
+                    
+                    break;
+                   
+                case 2:
+                    // actualizar empleado por documento
+                    System.out.println("Has elegido la opción: Actualizar Empleado");
+                    System.out.println("Ingrese el documento del empleado a actualizar: ");
+                    Scanner scannerActualizar = new Scanner(System.in);
+                    String documentoActualizar = scannerActualizar.nextLine();
+                    // nombre
+                    System.out.print("Ingrese el nuevo nombre del empleado: ");
+                    String nuevoNombre = scannerActualizar.nextLine();
+                    
+                    System.out.print("Ingrese el nuevo email del empleado: ");
+                    String nuevoEmail = scannerActualizar.nextLine();
 
-                }
-                case 3 -> funeraria.mostrarClientes();
-                case 4 -> funeraria.mostrarEmpleados();
-                case 5 -> {
-                }
-                case 6 -> {
+                    // tipo cargo
+                    System.out.print("Ingrese el nuevo cargo del empleado (1. ADMINISTRADOR, 2. ATENCION_AL_CLIENTE, 3. ASISTENTE): ");
+                    String nuevoCargoStr = scannerActualizar.nextLine();
+                    TipoCargo nuevoCargo = null;
 
-                }
-                case 0 -> System.out.println("Saliendo...");
-                default -> System.out.println("Opción inválida.");
+                    if(nuevoCargoStr.equals("1") || nuevoCargoStr.equals("2") || nuevoCargoStr.equals("3")){
+                        // convertir a enum
+                       
+                        switch (nuevoCargoStr) {
+                            case "1":
+                                nuevoCargo = TipoCargo.AUXILIAR;
+                                break;
+                            case "2":
+                                nuevoCargo =TipoCargo.CONDUCTOR;
+                                break;
+                            case "3":
+                                nuevoCargo =TipoCargo.VENDEDOR;
+                                break;
+                        }
+
+                    }
+                    
+                    funeraria.actualizarEmpleado(documentoActualizar, nuevoNombre, nuevoEmail, nuevoCargo);
+                    
+                    // actualizar empleado
+                    //funeraria.actualizarEmpleado(documentoActualizar, nuevoNombre, nuevoEmail);
+
+
+                default:
+                    System.out.println("Opción no válida, intenta de nuevo.");
             }
-        } while (opcion != 0);
+            System.out.println();
 
-        sc.close();
+        } while (opcion != 4);
     }
+
+    private static void mostrarMenuCliente(Funeraria funeraria) {
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("===== MENÚ CLIENTE =====");
+            System.out.println("1. Ver Servicios Disponibles");
+            System.out.println("2. Ver Productos Disponibles");
+            System.out.println("3. Volver al Menú Principal");
+            System.out.print("Seleccione una opción: ");
+            
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Has elegido la opción: Ver Servicios Disponibles");
+                    // Aquí podrías llamar a métodos para mostrar servicios
+                    break;
+                case 2:
+                    System.out.println("Has elegido la opción: Ver Productos Disponibles");
+                    // Aquí podrías llamar a métodos para mostrar productos
+                    break;
+                case 3:
+                    
+                    System.out.println("Volviendo al Menú Principal...");
+
+                    break;
+                default:
+                    System.out.println("⚠ Opción no válida, intenta de nuevo.");
+            }
+            System.out.println();
+
+        } while (opcion != 3);
+    }
+
+    private static void mostrarMenuEmpleado(Funeraria funeraria) {
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("===== MENÚ EMPLEADO =====");
+            System.out.println("1. Registrar Cliente");
+            System.out.println("2. Eliminar Cliente");
+            System.out.println("3. Actualizar Cliente");            
+            System.out.println("4. Volver al Menú Principal");
+            System.out.print("Seleccione una opción: ");
+            
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Has elegido la opción: Registrar Cliente");
+                    Scanner scannerCliente = new Scanner(System.in);
+                    System.out.print("Ingrese el nombre del cliente: ");
+                    String nombreCliente = scannerCliente.nextLine();
+                    System.out.print("Ingrese el documento del cliente: ");
+                    String documentoCliente = scannerCliente.nextLine();
+                    System.out.print("Ingrese el email del cliente: ");
+                    String email = scannerCliente.nextLine();
+
+                    Cliente cliente = new Cliente.Builder()
+                            .nombre(nombreCliente)
+                            .identificacion(documentoCliente)
+                            .email(email)
+                            .fechaNacimiento(null)
+                            .deuda(0)
+                            .build();
+
+                    funeraria.registrarCliente(cliente);
+                    System.out.println("Cliente registrado exitosamente.");
+                    
+                    break;
+                case 2:
+                    // Eliminar cliente
+                    System.out.println("Ingresa el documento del cliente a eliminar: ");
+                    Scanner scannerEliminar = new Scanner(System.in);
+                    String documentoEliminar = scannerEliminar.nextLine();
+                    funeraria.eliminarCliente(documentoEliminar);
+                    break;
+
+                
+                case 3:
+                    // Actualizar cliente por documento
+                    System.out.println("Ingrese el documento del cliente a actualizar: ");
+                    Scanner scannerActualizar = new Scanner(System.in);
+                    String documentoActualizar = scannerActualizar.nextLine();
+                    // nombre
+                    System.out.print("Ingrese el nuevo nombre del cliente: ");
+                    String nuevoNombre = scannerActualizar.nextLine();
+                    // email
+                    System.out.print("Ingrese el nuevo email del cliente: ");
+                    String nuevoEmail = scannerActualizar.nextLine();
+
+                    funeraria.actualizarCliente(documentoActualizar, nuevoNombre, nuevoEmail);
+
+                    break;
+                case 4:
+                    System.out.println("Volviendo al Menú Principal...");
+                default:
+                    System.out.println("⚠ Opción no válida, intenta de nuevo.");
+            }
+            System.out.println();
+
+        } while (opcion != 4);
+        }
+
+    private static Boolean iniciarSesion() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese su nombre de usuario: ");
+        String username = sc.nextLine();
+        System.out.print("Ingrese su contraseña: ");
+        String password = sc.nextLine();
+
+        // Aquí podrías agregar la lógica para verificar las credenciales
+        System.out.println("Iniciando sesión para el usuario: " + username);
+        return true;
+    }    
 }
     
--- Funeraria/src/main/java/co/edu/uniquindio/poo/model/Funeraria.java
+++ Funeraria/src/main/java/co/edu/uniquindio/poo/model/Funeraria.java
@@ -46,16 +46,97 @@ public class Funeraria {
     }
 
     public void registrarCliente(Cliente cliente) {
+        if (!this.listClientes.contains(cliente)) {
+            listClientes.add(cliente);
+            System.out.println("Cliente registrado con exito.");
+        }else{
+            System.out.println("El cliente ya existe");
+        }
 
     }
 
+    // eliminar cliente por documento
+    public void eliminarCliente(String documento) {
+        Cliente clienteAEliminar = null;
+        for (Cliente cliente : listClientes) {
+            if (cliente.getIdentificacion().equals(documento)) {
+                clienteAEliminar = cliente;
+                break;
+            }
+        }
+        if (clienteAEliminar != null) {
+            listClientes.remove(clienteAEliminar);
+            System.out.println("Cliente eliminado con exito.");
+        } else {
+            System.out.println("El cliente a eliminar no existe");
+        }
+    }
+
+
+    public void actualizarCliente(String documento, String nuevoNombre, String nuevoEmail) {
+        Cliente clienteAActualizar = null;
+        for (Cliente cliente : listClientes) {
+            if (cliente.getIdentificacion().equals(documento)) {
+                clienteAActualizar = cliente;
+                break;
+            }
+        }
+        if (clienteAActualizar != null) {
+            clienteAActualizar.setNombre(nuevoNombre);
+            clienteAActualizar.setEmail(nuevoEmail);
+            System.out.println("Cliente actualizado con exito.");
+        } else {
+            System.out.println("El cliente a actualizar no existe");
+        }
+    }
     public void registrarEmpleado(Empleado empleado) {
+
+        if (!this.listEmpleados.contains(empleado)) {
+            listEmpleados.add(empleado);
+            System.out.println("Empleado registrado con exito.");
+        }else{
+            System.out.println("El empleado ya existe");
+        }
     }
 
     public void mostrarClientes() {
+        if (listClientes.isEmpty()) {
+            System.out.println("No hay clientes registrados.");
+        } else {
+            System.out.println("Lista de clientes:");
+            for (Cliente cliente : listClientes) {
+                System.out.println(cliente);
+            }
+        }
     }
 
     public void mostrarEmpleados() {
+        if (listEmpleados.isEmpty()) {
+            System.out.println("No hay empleados registrados.");
+        } else {
+            System.out.println("Lista de empleados:");
+            for (Empleado empleado : listEmpleados) {
+                System.out.println(empleado);
+            }
+        }
+    }
+
+    public void actualizarEmpleado(String documento, String nuevoNombre, String nuevoEmail, TipoCargo nuevoCargo) {
+        Empleado empleadoAActualizar = null;
+        for (Empleado empleado : listEmpleados) {
+            if (empleado.getIdentificacion().equals(documento)) {
+                empleadoAActualizar = empleado;
+                break;
+            }
+        }
+        if (empleadoAActualizar != null) {
+            empleadoAActualizar.setNombre(nuevoNombre);
+            empleadoAActualizar.setEmail(nuevoEmail);
+            empleadoAActualizar.setCargo(nuevoCargo);
+            System.out.println("Empleado actualizado con exito.");
+        } else {
+            System.out.println("El empleado a actualizar no existe");
+        }
     }
 
     //public
